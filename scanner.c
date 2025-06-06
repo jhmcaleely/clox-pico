5855762f177b793927c03bd5feb8363471df3941
John McAleely <john@mcaleely.com>
2025-06-06 16:13:53 +0100

    Introduce peek() function and poke address,value statement
    peek() is implemented as a native function via defineNative. Takes an address, and returns the 32 bit value stored there.
    
    poke address, value;
    
    Statement added alongside print to write a value to a specified address.

--- scanner.c
+++ scanner.c
@@ -145,7 +145,14 @@ static TokenType identifierType() {
         case 'i': return checkKeyword(1, 1, "f", TOKEN_IF);
         case 'n': return checkKeyword(1, 2, "il", TOKEN_NIL);
         case 'o': return checkKeyword(1, 1, "r", TOKEN_OR);
-        case 'p': return checkKeyword(1, 4, "rint", TOKEN_PRINT); 
+        case 'p':
+            if (scanner.current - scanner.start > 1) {
+                switch (scanner.start[1]) {
+                    case 'o': return checkKeyword(2, 2, "ke", TOKEN_POKE);
+                    case 'r': return checkKeyword(2, 3, "int", TOKEN_PRINT);
+                }
+            }
+            break;
         case 'r': return checkKeyword(1, 5, "eturn", TOKEN_RETURN);
         case 's': return checkKeyword(1, 4, "uper", TOKEN_SUPER);
         case 't':
